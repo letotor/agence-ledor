ff1182ba423e5295cfc577cd8770d35a0a28cecd
victor DG <v.degermain@gmail.com>
2022-04-28 13:01:10 +0200

    ajout contact et footer

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('BIENVENU SUR AGENCE LEDOR')
+
+
+
+var form = document.getElementsByName('contact-form');
+var formValid = document.getElementsByName('submit')
+
+console.log(formValid)
